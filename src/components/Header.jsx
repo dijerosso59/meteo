8f3b51bd8a9e668a90a389b85be5faf3b970571f
luca di rosso <luca.di.rosso@dkgroup.fr>
2022-02-24 16:47:49 +0100

    though commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,10 @@ import logo from "../assets/images/logo.png";
 function Header() {
     return (
         <header>
-            <img src={logo} alt="logo" />
+            <Link to="/">
+                <img src={logo} alt="logo" />
+            </Link>
+
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/search">Rechercher</Link></li>
